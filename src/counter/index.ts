b6b53021649f9ff695a5bcbf31f6ba503678c1ba
Jonas Kello <jonas.kello@divid.se>
2018-09-02 17:06:22 +0200

    Remove index files

--- src/counter/index.ts
+++
@@ -1,9 +0,0 @@
-import { ReactProgram } from "raj-react";
-import { view } from "./counter-view";
-import { update, State } from "./counter-state";
-import { Action } from "./counter-actions";
-
-export const program: ReactProgram<State, Action> = {
-  update,
-  view
-};
